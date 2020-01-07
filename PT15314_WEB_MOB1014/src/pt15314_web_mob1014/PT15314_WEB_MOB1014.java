17fc221489ee08721109eb16f3ebde05a3344601
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-01-07 15:29:50 +0700

    File Lop PT15314

---
+++ PT15314_WEB_MOB1014/src/pt15314_web_mob1014/PT15314_WEB_MOB1014.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt15314_web_mob1014;
+
+/**
+ *
+ * @author LegendNguyen
+ */
+public class PT15314_WEB_MOB1014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+    }
+    
+}
